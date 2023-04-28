2909781ed01c856e31c7bc9f3107a8103cdb4830
htn9sys <hatsune.927@gmail.com>
2023-04-28 14:54:22 +0900

    ユーザー管理機能の実装_userモデル単体のテストコード作成

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,103 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'nickname、email、password、password_confirmation、family_name、given_name、family_name_kana、given_name_kana、birthdayが存在すれば登録できる' do
+      expect(@user).to be_valid
+    end
+    it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+    it 'family_nameが空では登録できない' do
+      @user.family_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name can't be blank")
+    end
+    it 'given_nameが空では登録できない' do
+      @user.given_name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Given name can't be blank")
+    end
+    it 'family_name_kanaが空では登録できない' do
+      @user.family_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name kana can't be blank")
+    end
+    it 'given_name_kanaが空では登録できない' do
+      @user.given_name_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Given name kana can't be blank")
+    end
+    it 'birthdayが空では登録できない' do
+      @user.birthday = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
+    end
+    it 'nicknameが40文字以上では登録できない' do
+      @user = FactoryBot.build(:user, nickname: Faker::Lorem.characters(number: 41))
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname is too long (maximum is 40 characters)")
+    end
+    it '重複したemailが存在する場合は登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+    it 'emailは@を含まないと登録できない' do
+      @user.email = 'testmail'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Email is invalid')
+    end
+    it 'passwordが5文字以下では登録できない' do
+      @user.password = '12345'
+      @user.password_confirmation = '12345'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+    it 'passwordは英語だけでは登録できない' do
+      @user.password = 'password'
+      @user.password_confirmation = 'password'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+    end
+    it 'passwordは数字だけでは登録できない' do
+      @user.password = '12345678'
+      @user.password_confirmation = '12345678'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+    end
+    it 'family_name_kanaはカタカナでないと登録できない' do
+      @user.family_name_kana = 'test', 'てすと'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name kana is invalid. Input full-width katakana characters")
+    end
+    it 'given_name_kanaはカタカナでないと登録できない' do
+      @user.given_name_kana = 'test', 'てすと'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Given name kana is invalid. Input full-width katakana characters")
+    end
+  end
+end
