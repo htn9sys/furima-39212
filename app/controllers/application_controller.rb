1424edcecb83b1f36a58f59328e95dcc81a5832c
htn9sys <hatsune.927@gmail.com>
2023-05-09 22:43:21 +0900

    render向け日本語対応実装_修正2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :given_name, :family_name_kana, :given_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :given_name, :family_name_kana, :given_name_kana, :birth_date])
   end
   
 end
